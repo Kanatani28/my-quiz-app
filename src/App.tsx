c7468597b80188857a0f481369cd41cf3cabfdef
MasahiroKanatani <5genbassnohito.maroro0208@gmail.com>
2021-02-16 11:58:18 +0900

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { Question } from './@types';
 import questions from './data'
 
 function App() {
@@ -8,30 +7,56 @@ function App() {
   const [count, setCount] = useState(initialCount)
   const [visible, setVisible] = useState(false)
 
+  
+  const [checkList, setCheckList] = useState<string[]>([])
+  const [isCorrect, setCorrect] = useState(false)
+  
+  function showAnswer() {
+    setCorrect(checkList.some(c => c == questions[count].answer?.toString()))
+    setVisible(!visible)
+  }
+  
+  function checkboxChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const value = e.target.value
+    if(checkList.some(c => c === e.target.value)) {
+      setCheckList(checkList.filter(c => value !== c))
+    } else {
+      setCheckList([...checkList, value])
+    }
+  }
+
   function onNext() {
     localStorage.setItem("count", (count+1).toString())
     setCount(count+1)
     setVisible(false)
+    setCheckList([])
+    setCorrect(false)
   }
   
   function onPrev() {
     localStorage.setItem("count", (count-1).toString())
     setCount(count-1)
-    setVisible(false) 
+    setVisible(false)
+    setCheckList([])
+    setCorrect(false)
   }
   return (
     <div style={{ padding: "5%"}}>
-      <button onClick={() => setVisible(!visible)}>解説をみる</button>
+      <div>問{ count + 1 }</div>
+      <button onClick={() => showAnswer()}>解説をみる</button>
       <button disabled={count === 0} onClick={() => onPrev()}>前へ</button>
       <button disabled={count === questions.length - 1} onClick={() => onNext()}>次へ</button>
       <div style={{ whiteSpace: "pre-wrap"}}>{questions[count].sentence}</div>
       <ul>
-        {questions[count].choices.map(c => 
-          <li style={{margin: "10px"}}>{c.text}</li>
+        {questions[count].choices.map((c, i) => 
+          <li style={{margin: "10px"}}><input type="checkbox" checked={checkList.includes(i.toString())} value={i} onChange={(e) =>  checkboxChange(e) } />{c.text}</li>
         )}
       </ul>
-      {visible &&
-        <div style={{ whiteSpace: "pre-wrap"}}>{ questions[count].explanation }</div>
+      {visible && 
+        <>
+          {isCorrect ? <div style={{backgroundColor: "greenyellow"}}>正解</div> : <div  style={{backgroundColor: "red"}}>不正解</div> }
+          <div style={{ whiteSpace: "pre-wrap"}}>{ questions[count].explanation }</div>
+        </>
       }
       
     </div>
